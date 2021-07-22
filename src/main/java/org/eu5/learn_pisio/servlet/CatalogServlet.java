cd94adf3475f82d7d399a567db1fa09f14cdd780
Dragan Cajic <cajic_dragan@yahoo.com>
2021-07-22 13:31:21 +0200

    generate response back to browser

--- src/main/java/org/eu5/learn_pisio/servlet/CatalogServlet.java
+++ src/main/java/org/eu5/learn_pisio/servlet/CatalogServlet.java
@@ -1,8 +1,11 @@
 package org.eu5.learn_pisio.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,7 +39,45 @@ public class CatalogServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		// collect information from the HTTP request [get parameters from the form] 
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+		
+		// add an item to the catalog
+		Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+		
+		// ~ generate response back to the browser
+		
+		// set header and add cookie
+		response.setHeader("someHeader", "someHeaderValue");
+		response.addCookie(new Cookie("someCookie", "someCookieValue"));
+		
+		// start generating the actual response body
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("\t<meta charset=\"utf-8\" />");
+		out.println("\t<title>H+ Sport Catalog</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("\t<table>");
+		
+		for (CatalogItem item : Catalog.getItems()) {
+			out.println("\t\t<tr>");
+			out.println("\t\t\t<td>");
+			out.println("\t\t\t\t" + item.getName());
+			out.println("\t\t\t</td>");
+			out.println("\t\t</tr>");
+		}
+		
+		out.println("\t</table>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
